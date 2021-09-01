2e748c0019a14c441d9ac8734a88b7adefde2f24
Julie Meng <julieannmeng@gmail.com>
2021-09-01 12:54:07 -0400

    commit tree 1f0260bd0621ab56edb87129c012efde6b9f82b0

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,13 @@
 function Article({title, date="January 1, 1970", preview, minutes}) {
     return (
-        <Article>
+        <div>
             <h3>{title}</h3>
             <small>
                 {date} {minutes} min read
             </small>
             <p>{preview}</p>
             <p></p>
-        </Article>
+        </div>
     );
 }
 
